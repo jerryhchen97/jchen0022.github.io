f5a17953eff416f1d22bb52ec88272dc8b05a55a
Jerry Chen <jchen0022@berkeley.edu>
2017-09-19 22:22:56 -0700

    Projects under construction

--- index.js
+++ index.js
@@ -22,9 +22,25 @@ function proj2Toggle(e) {
     $("#proj2").toggle("slide", {direction: "right"}, 500);
 }
 
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
 $(document).ready(function() {
     $(".about-toggle").click(moveAbout);
     $(".projects-toggle").click(moveProjects);
     $(".proj1-toggle").click(proj1Toggle);
     $(".proj2-toggle").click(proj2Toggle);
 });
+
